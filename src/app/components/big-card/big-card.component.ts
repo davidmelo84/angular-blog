44e556c4835ab00fed7a5900ad1618a0c7946fcf
David <melodavid84@icloud.com>
2024-01-19 12:43:55 -0300

    Adicionando propiedades dinamicas

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit{
+
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = "SAIBA MAIS SOBRE DAVID"
+  @Input()
+  cardDescription:string = "Programador Front End, focado , David girou a chave da sua vida profissional proximo aos 40 anos, quando decidiu entrar de cabeça no mundo dos códigos e desenvolvimento."
+
+  constructor(){}
+  ngOnInit(): void {
+
+  }
 
 }
